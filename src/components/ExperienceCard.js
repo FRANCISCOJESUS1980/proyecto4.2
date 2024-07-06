28827615f6b99872e0b928eb2616329138974bf3
FRANCISCOJESUS1980 <jesusgonzalezvergara@hotmail.com>
2024-07-06 15:48:12 +0200

    feat creando hobbies y experiencias

---
+++ src/components/ExperienceCard.js
@@ -0,0 +1,28 @@
+import './ExperienceCard.scss'
+
+export function ExperienceCard() {
+  const card = document.createElement('article')
+  card.className = 'card card--experience'
+
+  card.innerHTML = `
+    <h2 class="card__title">Experiencies</h2>
+    <div class="experience">
+      <img src="./assets/images.jpg/logo tienda.jpg" alt="logo" class="experience__image">
+      <div class="experience__info">
+        <p class="experience__time">Enero 2000 - Current</p>
+        <h3 class="experience__job">Gerente Mobiliario</h3>
+        <p class="experience__description">Tengo varias tiendas de muebles y electrodomesticos desde el año 2000 a la actualidad y basicamente vendemos de todo para el hogar</p>
+      </div>
+    </div>
+    <div class="experience">
+      <img src="./assets/images.jpg/logopower.jpg" alt="logo" class="experience__image">
+      <div class="experience__info">
+        <p class="experience__time">Marzo 2024 - actualidad</p>
+        <h3 class="experience__job">Estudiante ThePower</h3>
+        <p class="experience__description">Desde el 27 de Marzo de 2024 estoy asistiendo a clases de programacion en un master online en una de las mejores empresas del mundo, donde te proporcionan unas herramientas y conocimientos para estar muy por encima de la media en este sector</p>
+      </div>
+    </div>
+  `
+
+  return card
+}
---
+++ src/components/HobbiesCard.js
@@ -0,0 +1,36 @@
+import './HobbiesCard.scss'
+
+export function HobbiesCard() {
+  const card = document.createElement('article')
+  card.className = 'card card--hobbies'
+
+  card.innerHTML = `
+    <h2 class="card__title">Hobbies</h2>
+    <div class="hobby">
+      <img src="./assets/images.jpg/volar.jpg" alt="volar" class="hobby__image">
+      <div class="hobby__info">
+        <p class="hobby__description">Me gusta volar en parapente y explorar nuevos lugares desde el aire.</p>
+      </div>
+    </div>
+    <div class="hobby">
+      <img src="./assets/images.jpg/tiro.jpg" alt="tiro" class="hobby__image">
+      <div class="hobby__info">
+        <p class="hobby__description">Disfruto del tiro con arco y la precisión que requiere este deporte.</p>
+      </div>
+    </div>
+    <div class="hobby">
+      <img src="./assets/images.jpg/fortnite.jpg" alt="fortnite" class="hobby__image">
+      <div class="hobby__info">
+        <p class="hobby__description">Soy aficionado a los videojuegos, especialmente a Fortnite.</p>
+      </div>
+    </div>
+    <div class="hobby">
+      <img src="./assets/images.jpg/crosfit.JPG" alt="crosfit" class="hobby__image">
+      <div class="hobby__info">
+        <p class="hobby__description">Practico CrossFit para mantenerme en forma y desafiarme físicamente.</p>
+      </div>
+    </div>
+  `
+
+  return card
+}
