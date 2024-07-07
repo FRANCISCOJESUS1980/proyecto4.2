b404937b5ad3937045c513745f40407c09d47d1c
FRANCISCOJESUS1980 <jesusgonzalezvergara@hotmail.com>
2024-07-07 12:08:16 +0200

    feat terminado en html

--- src/main.js
+++ src/main.js
@@ -1,16 +1,22 @@
-import { Header } from './components/header.js'
-import { ProfileCard } from './components/ProfileCard'
-import { ExperienceCard } from './components/ExperienceCard'
-import { HobbiesCard } from './components/HobbiesCard'
-import { SkillsCard } from './components/SkillsCard'
-import { ProjectCard } from './components/ProjectCard'
-import './main.scss'
+import './components/main.scss'
+const toggleTheme = document.getElementById('toggle-theme')
+const toggleIcon = document.getElementById('toggle-icon')
+const toggleText = document.getElementById('toggle-text')
 
-const app = document.getElementById('app')
+const toggleColors = document.getElementById('toggle-colors')
+const rootStyles = document.documentElement.style
 
-app.appendChild(Header())
-app.appendChild(ProfileCard())
-app.appendChild(ExperienceCard())
-app.appendChild(HobbiesCard())
-app.appendChild(SkillsCard())
-app.appendChild(ProjectCard())
+toggleTheme.addEventListener('click', () => {
+  document.body.classList.toggle('dark')
+  if (toggleIcon.src.includes('moon.svg')) {
+    toggleIcon.src = './assets/icons.svg/soleado.png'
+    toggleText.textContent = 'Light Mode'
+  } else {
+    toggleIcon.src = 'assets/icons.svg/moon.svg'
+    toggleText.textContent = 'Dark Mode'
+  }
+})
+
+toggleColors.addEventListener('click', (e) => {
+  rootStyles.setProperty('--primary-color', e.target.dataset.color)
+})
